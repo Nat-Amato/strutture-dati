8aa39426e85059d8de2358b1fb316a9b2ce65209
N. Amato <48321178+Nat-Amato@users.noreply.github.com>
2020-10-23 08:40:44 +0200

    Add files via upload

---
+++ ASD - Lab3/matrice.cpp
@@ -0,0 +1,12 @@
+// costruttore
+matrice::matrice(int c, int r){
+  colonne = c;
+  righe = r;
+  // allocazione dinamica della matrice
+  int i;
+  elementi = new tipoelem* [righe];
+  for (i=0; i < righe; i++)
+    elementi[i] = new tipoelem[colonne];
+  // inizializzazione degli elementi
+     ...
+}
---
+++ ASD - Lab3/matrice.h
@@ -0,0 +1,16 @@
+definire il tipo tipoelem
+
+	typedef double tipoelem;
+
+definire la classe matrice
+
+class matrice {
+  public:
+    matrice(int, int); /* costruttore */
+    tipoelem leggimatrice(int, int);
+    void scrivimatrice(int, int, tipoelem);
+  private:
+    int righe;
+    int colonne;
+    tipoelem **elementi;
+};
