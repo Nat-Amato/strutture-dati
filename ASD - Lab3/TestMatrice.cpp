69292e9a1e9d94c8df378d60198957dd1702120a
N. Amato <48321178+Nat-Amato@users.noreply.github.com>
2020-10-23 09:04:42 +0200

    Update TestMatrice.cpp

--- ASD - Lab3/TestMatrice.cpp
+++ ASD - Lab3/TestMatrice.cpp
@@ -6,3 +6,18 @@ int main() {
   
 	return 0;
 }
+
+matrice.h : contiene solo la def della classe
+matrice.cpp: implementazione
+main: test della matrice
+
+nella parte private: la matrice è un tipoelem, un array di puntatori, inizializzato nel costruttore con due input. dopo l'allocazione
+--> [...]
+--> [...]
+...
+--> [...]
+
+elementi[i] [j]
+inizializzati gli elementi a 0
+	
+Volendo invece di definire un alias di double, possiamo già definire la classe di tipoelem
