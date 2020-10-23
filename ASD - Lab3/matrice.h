d25ccd407ec330068ce8734ed17c4ebc055462cf
Franco91 <48320848+Franco91@users.noreply.github.com>
2020-10-23 19:09:59 +0200

    Add files via upload

--- ASD - Lab3/matrice.h
+++ ASD - Lab3/matrice.h
@@ -1,13 +1,10 @@
 #ifndef _MATRICE
 #define _MATRICE
 
-using namespace std;
-
-definire il tipo tipoelem
+	using namespace std;
 
 	typedef double tipoelem;
 
-definire la classe matrice
 
 class matrice {
 
@@ -15,30 +12,32 @@ class matrice {
   
     matrice(int, int); 					/* costruttore */
     
-    matrice(const matrice& M){
-	matrice(M.righe, M.colonne);
-	//...	
-    }							/* costruttore di copia */  
+	matrice(const matrice&);					/* costruttore di copia */
     
-    matrice& operator=(const matrice& M){
-    	//come per il costruttore di copia
-	//...
-    	return *this;
-    }							/* operatore di assegnamento */
+	matrice& operator=(const matrice&);						/* operatore di assegnamento */
     
     ~matrice();  					 /* distruttore */
-    
+   
     tipoelem leggimatrice(int, int);
     
     void scrivimatrice(int, int, tipoelem);
+
+	void prodottoScalare(double);
+
+	matrice& trasposta(const matrice&);
+
+	matrice& prodotto(matrice&);
     
   private:
     int righe;
     int colonne;
     tipoelem **elementi;
+
+	tipoelem opInterne(matrice&, matrice&, int, int);
 };
 
 #endif /* _MATRICE */
+
 
 
 
